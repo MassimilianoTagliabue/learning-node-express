b90dfcd44219e64ac9507a899749524b578c8039
Massimiliano <massimilianotagliabue.04@gmail.com>
2026-02-05 16:45:12 +0100

    learn: request params

--- Learning-Web-Server/server.js
+++ Learning-Web-Server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-const user = [
+const users = [
     {id:1, nome:'ciao'},
     {id:2, nome:'pino'},
     {id:3, nome:'pippo'}
@@ -22,8 +22,24 @@ app.get('/about', (req,res) => {
 
 app.get('/api/users', (req, res) => {
 
-    res.json(user)
+    res.json(users)
 })
+
+//Get single post
+app.get('/api/users/:id', (req,res) => {
+
+    const id = parseInt(req.params.id); //andiamo a prendere l'id dall'url tramite il comando params
+    const user = users.find((user) => user.id === id);
+
+    if(!user){
+        res.status(404).json({msg : "utente non trovato"})
+    }else{
+        res.status(200).json(user);
+    }
+
+})
+
+
 
 
 app.use((req,res) => {
